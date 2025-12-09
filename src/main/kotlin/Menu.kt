fcd0330933d89917f96fdee292444bc73871d83d
Siddqd <61403599+Siddqd@users.noreply.github.com>
2025-12-09 13:52:28 +0400

    Update Menu.kt

--- src/main/kotlin/Menu.kt
+++ src/main/kotlin/Menu.kt
@@ -1,12 +1,23 @@
-class Menu(val dataList : MutableList<T>) {
-  private val lambdaList = mutableListOf<Pair<String, () -> Unit>>()
+class Menu() {
+  open val type : String
+  open val mlist : MutableList<Item> = mutableListOf()
+  open val actionList = mutableListOf<Pair<Int, () -> Unit>>()
   
   fun showMenu() {
-    
+    if (!lambdaList.isEmpty()) {
+      lambdaList.forEach(println(lambdaList.first))
+        println(lambdaList.first)
+    }
   }
-  fun selectMenu() {
-    val cmd : String = readln
+  fun selectMenu(nom : Int) {
+    val action = actionList.find { it.first == nom }
+    if (action != null) {
+        action.second()
+    } else {
+        println("Действие '$name' не найдено!")
+    }
   }
 
+      
 
 }
