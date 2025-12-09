86c4ad14d741cc32c41b4bf3553190e71898e663
Siddqd <61403599+Siddqd@users.noreply.github.com>
2025-12-09 11:33:04 +0400

    Create Menu.kt

---
+++ src/main/kotlin/Menu.kt
@@ -0,0 +1,12 @@
+class Menu(val dataList : MutableList<T>) {
+  private val lambdaList = mutableListOf<Pair<String, () -> Unit>>()
+  
+  fun showMenu() {
+    
+  }
+  fun selectMenu() {
+    val cmd : String = readln
+  }
+
+
+}
